42d2c9cae21cf4b93ba468bc5fc9fd157d50aa4e
Youssef <youssef_nassar98@outlook.com>
2025-03-22 12:56:16 -0700

    first release of mobile version of the site

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+const navElement = document.querySelector('.site_nav')
+const hamburger_element = document.querySelector('.hamburger')
+
+hamburger_element.addEventListener('click', () => {
+    navElement.classList.toggle("site_nav_open")
+    hamburger_element.classList.toggle("hamburger_open")
+})
+
+navElement.addEventListener('click', () => {
+    navElement.classList.remove("site_nav_open")
+    hamburger_element.classList.remove("hamburger_open")
+})
